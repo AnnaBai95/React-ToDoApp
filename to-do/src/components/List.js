ec3cd72c3a93ff1a1df6d0fcdb41292787f99ae6
AnnaBai95 <annastacia_bailey@yahoo.com>
2023-10-31 21:12:54 -0500

    Responsiveness + Complete sections
    Handled the responsiveness of the page with media queries and completed adding the components for the page. The checkbox border hover is not yet completed

--- to-do/src/components/List.js
+++ to-do/src/components/List.js
@@ -2,32 +2,35 @@ import '../styles/list.css';
 
 export default function List() {
     return (
-        <div className="list-box">
-            <div className="item-list">
-                <div className="item">
-                    <div className="rounded-check">
-                        <input type="checkbox" id="item1"></input>
-                        <label htmlFor="item1"></label>
+        <>
+            <div className="list-box">
+                <div className="item-list">
+                    <div className="item">
+                        <div className="rounded-check">
+                            <input type="checkbox" id="item1"></input>
+                            <label htmlFor="item1"></label>
+                        </div>
+                        <p>Complete online javascript course</p>
                     </div>
-                    <p>Complete online javascript course</p>
+                    <div className="item">
+                        <div className="rounded-check">
+                            <input type="checkbox" id="item2"></input>
+                            <label htmlFor="item2"></label>
+                        </div>
+                        <p>Jog around the park three times</p>
+                    </div>
                 </div>
-                <div className="item">
-                    <div className="rounded-check">
-                        <input type="checkbox" id="item2"></input>
-                        <label htmlFor="item2"></label>
+                <div className="list-footer">
+                    <span>5 items left</span>
+                    <div className="flex-gap text-bold">
+                        <a className="active-link links">All</a>
+                        <a className="links">Active</a>
+                        <a className="links">Completed</a>
                     </div>
-                    <p>Jog around the park three times</p>
+                    <a className="links">Clear Completed</a>
                 </div>
             </div>
-            <div className="list-footer">
-                <span>5 items left</span>
-                <div className="flex-gap">
-                    <a>All</a>
-                    <a>Active</a>
-                    <a>Completed</a>
-                </div>
-                <a>Clear Completed</a>
-            </div>
-        </div>
+            <p class="instructions">Drag and drop to reorder list</p>
+        </>
     );
 }
